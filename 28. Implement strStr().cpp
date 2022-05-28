be86dad8bf86af59ec92d26b9d35d04cb0ad2c2b
MRIDUL <77963406+Mridul1911@users.noreply.github.com>
2022-05-28 11:04:35 +0530

    Create 28. Implement strStr().cpp

---
+++ 28. Implement strStr().cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool check(string a,string b){
+        return a==b;
+    }
+    int strStr(string haystack, string needle) {
+        if(haystack==needle) return 0;
+        int n = needle.size();
+        int m = haystack.size();
+        for(int i=0;i<=m-n;i++){
+            if(check(haystack.substr(i,n),needle)) return i;
+        }
+        return -1;
+    }
+};
