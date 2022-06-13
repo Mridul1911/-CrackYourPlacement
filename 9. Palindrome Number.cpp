c56b16eee479a98cb74ed9e7719803242766971c
MRIDUL <77963406+Mridul1911@users.noreply.github.com>
2022-06-13 13:25:48 +0530

    Create 9. Palindrome Number.cpp

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,14 @@
+// Reverse the second half of the number and then compare.
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int rev = 0;
+        while (rev < x) {
+            rev = rev * 10 + x % 10;
+            x /= 10;     
+        }
+        return x == rev || x == rev / 10;
+    }
+};
